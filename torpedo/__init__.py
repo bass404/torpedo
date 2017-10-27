7f25fb373b1b4a810c9432b861dc7276b8d5e1c7
Diwas Sharma <diwasblack@gmail.com>
2017-10-27 09:27:07 -0500

    Added mongodb client for flask

--- torpedo/__init__.py
+++ torpedo/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_login import LoginManager
+from pymongo import MongoClient
 
 torpedo_app = Flask(__name__, static_url_path='/static')
 torpedo_app.config.from_object('torpedo.config')
+
+login_manager = LoginManager()
+login_manager.init_app(torpedo_app)
+
+mongodb_client = MongoClient('localhost', 27017)
 
 # Import views at end to avoid cyclic dependency errors
 from torpedo.products import views
--- torpedo/products/views.py
+++ torpedo/products/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from torpedo import torpedo_app
+from torpedo import mongodb_client
 
 
 @torpedo_app.route('/')
