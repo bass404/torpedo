10ce9739cdf078a325b37b9be6320dd5affa177b
Diwas Sharma <diwasblack@gmail.com>
2017-10-31 11:08:14 -0500

    Updated url name

--- torpedo/__init__.py
+++ torpedo/__init__.py
@@ -15,5 +15,5 @@ connect(
 )
 
 # Import views at end to avoid cyclic dependency errors
-from torpedo.products import views
-from torpedo.users import views
+from torpedo.products import views as product_views
+from torpedo.users import views as user_views
