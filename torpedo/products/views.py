bb6a68f245ddc047488b8eb2ef3b2c7710da27e1
Diwas Sharma <diwasblack@gmail.com>
2017-11-01 09:13:34 -0500

    Added template for product detail view

--- torpedo/products/views.py
+++ torpedo/products/views.py
@@ -6,3 +6,8 @@ from torpedo import torpedo_app
 @torpedo_app.route("/products", methods=["GET", "POST"])
 def products_list_view():
     return render_template("products/list.html")
+
+
+@torpedo_app.route("/product/detail", methods=["GET"])
+def product_detail_view():
+    return render_template("products/detail.html")
