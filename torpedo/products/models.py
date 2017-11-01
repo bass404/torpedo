449a6f49deb9aab5c95640c9e1017ef58bab208f
Diwas Sharma <diwasblack@gmail.com>
2017-11-01 18:08:00 -0500

    Added models for products

---
+++ torpedo/products/models.py
@@ -0,0 +1,26 @@
+from mongoengine import (
+    Document, StringField, ReferenceField, CASCADE, FloatField, IntField
+)
+
+
+class Category(Document):
+    name = StringField()
+    description = StringField()
+
+
+class Product(Document):
+    name = StringField()
+    description = StringField()
+    category = ReferenceField("products.Category", reverse_delete_rule=CASCADE)
+
+
+class ProductAttribute(Document):
+    product = ReferenceField("products.Product", reverse_delete_rule=CASCADE)
+    size = StringField()
+    color = StringField()
+
+    # TODO handle pictures
+
+    price = FloatField()
+    discount = FloatField()
+    stock = IntField()
