9a88017c0b9696c25cfec604ed240796395bca4b
Diwas Sharma <diwasblack@gmail.com>
2017-11-01 20:14:26 -0500

    Updated add products views to include category selection

--- torpedo/products/forms.py
+++ torpedo/products/forms.py
@@ -1,8 +1,25 @@
 from flask_wtf import Form
 
-from wtforms import TextField, StringField
+from wtforms import TextField, StringField, SelectField
+
+from torpedo.products.models import Category
 
 
 class ProductForm(Form):
     name = StringField()
     description = TextField()
+    category = SelectField(choices=[('test', 'test 1')])
+
+    def __init__(self):
+        super().__init__()
+
+        # TODO verify if this works as expected
+
+        # Obtain choices for categories
+        categories = Category.objects()
+
+        category_choices = [
+            (str(category.id), category.name) for category in categories
+        ]
+
+        self.category.choices = category_choices
--- torpedo/products/views/admin_views.py
+++ torpedo/products/views/admin_views.py
@@ -1,14 +1,33 @@
-from flask import render_template, abort
-from flask_login import current_user
+from flask import render_template, abort, flash, request, redirect, url_for
+from flask_login import current_user, login_required
 
 from torpedo import torpedo_app
+from torpedo.products.models import Product, Category
 from torpedo.products.forms import ProductForm
 
 
 @torpedo_app.route("/products/admin/add", methods=["GET", "POST"])
+@login_required
 def add_product_view():
     if not current_user.is_admin:
         abort(403)
 
     form = ProductForm()
-    return render_template("products/admin/add.html", form=form, heading="Add product")
+    if request.method == "POST":
+        if form.validate_on_submit():
+            category = Category.objects(id=form.data["category"])[0]
+
+            product = Product(
+                name=form.data["name"],
+                description=form.data["description"],
+                category=category.id
+            )
+            product.save()
+
+            flash("Product added")
+            return redirect(url_for('user_setting_view'))
+        else:
+            flash("Form validation error")
+            return render_template("products/admin/add.html", form=form, heading="Add product")
+    else:
+        return render_template("products/admin/add.html", form=form, heading="Add product")
