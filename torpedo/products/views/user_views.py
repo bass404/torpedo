feba2cc4247859f70fa43400264f4309423946df
Diwas Sharma <diwasblack@gmail.com>
2017-11-01 19:10:16 -0500

    Seperate folders for products views

--- torpedo/products/views/user_views.py
+++ torpedo/products/views/user_views.py
@@ -3,11 +3,11 @@ from flask import render_template
 from torpedo import torpedo_app
 
 
-@torpedo_app.route("/products", methods=["GET", "POST"])
+@torpedo_app.route("/products/list", methods=["GET", "POST"])
 def products_list_view():
     return render_template("products/list.html")
 
 
-@torpedo_app.route("/product/detail", methods=["GET"])
+@torpedo_app.route("/products/detail", methods=["GET"])
 def product_detail_view():
     return render_template("products/detail.html")
