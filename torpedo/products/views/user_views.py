9fa9a846381254c95807ecb83e68ca15ddaa74cf
Diwas Sharma <diwasblack@gmail.com>
2017-11-08 15:52:11 -0600

    Added optional attribute_id fetch to the product model

--- torpedo/products/views/user_views.py
+++ torpedo/products/views/user_views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request
+from flask import render_template, request
 
 from torpedo import torpedo_app
 from torpedo.products.models import Product
@@ -8,11 +8,22 @@ from torpedo.products.models import Product
 def products_list_view():
     if request.method == "GET":
         products = Product.objects()
-        return render_template("products/list.html",heading="List of products", products=products)
+        return render_template("products/list.html", heading="List of products", products=products)
 
 
-@torpedo_app.route("/products/detail/<product_id>", methods=["GET","POST"])
-def product_detail_view(product_id):
+@torpedo_app.route("/products/detail/<product_id>/", methods=["GET", "POST"])
+@torpedo_app.route("/products/detail/<product_id>/<attribute_id>", methods=["GET", "POST"])
+def product_detail_view(product_id, attribute_id=None):
+    # Try to obtain the first item
     product = Product.objects(id=product_id).first()
 
-    return render_template("products/detail.html",product=product)
+    if attribute_id:
+        attribute = product.attributes.filter(id=attribute_id).first()
+
+        if not attribute:
+            attribute = product.attributes.filter().first()
+
+    else:
+        attribute = product.attributes.filter().first()
+
+    return render_template("products/detail.html", product=product, attribute=attribute)
