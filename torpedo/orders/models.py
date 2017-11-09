dec621a0e9245d396fb07f6cf1c0ef2bec7e37e1
Diwas Sharma <diwasblack@gmail.com>
2017-11-09 12:35:59 -0600

    Initial models for order app

---
+++ torpedo/orders/models.py
@@ -0,0 +1,61 @@
+from mongoengine import (
+    Document, EmbeddedDocument, ReferenceField, FloatField, IntField,
+    EmbeddedDocumentListField, EmbeddedDocumentField
+)
+
+
+class ProductAndAttribute(EmbeddedDocument):
+    """
+    EmbeddedDocument model to store product id and attribute id together
+    """
+
+    product = ReferenceField("products.Product")
+    product_attribute = ReferenceField("products.ProductAttribute")
+
+
+class BaseOrderDetail(EmbeddedDocument):
+    """
+    No collection will be created for this model
+    """
+
+    product_and_attribute = EmbeddedDocumentField(
+        ProductAndAttribute, required=True)
+    price = FloatField(required=True)
+    discount = FloatField(default=0.0)
+    quantity = IntField(default=1)
+
+    meta = {
+        "abstract": True,
+    }
+
+
+class CartProductDetail(BaseOrderDetail):
+    """
+    Store this model in seperate collection
+    """
+    pass
+
+
+class Cart(Document):
+    """
+    Model to hold the cart detail of a user
+    """
+
+    user = ReferenceField("users.User")
+    cart_product_details = EmbeddedDocumentListField(CartProductDetail)
+
+
+class OrderDetail(BaseOrderDetail):
+    """
+    Store this model in seperate collection
+    """
+    pass
+
+
+class Order(Document):
+    """
+    Model to hold the order detail for a user
+    """
+
+    user = ReferenceField("users.User")
+    cart_product_details = EmbeddedDocumentListField(OrderDetail)
