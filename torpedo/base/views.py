b51897a0c82f1333f627a01f31e982fbd8df6479
Diwas Sharma <diwasblack@gmail.com>
2017-11-16 18:14:19 -0600

    Retrive index page new released products for database

--- torpedo/base/views.py
+++ torpedo/base/views.py
@@ -1,11 +1,12 @@
 from flask import render_template
 
 from torpedo import torpedo_app
+from torpedo.products.helpers import get_products
 
 
 @torpedo_app.route('/')
 def index():
-    return render_template("base/index.html")
+    return render_template("base/index.html", new_released_products=get_products(6), featured=get_products(4))
 
 
 @torpedo_app.route("/contact")
